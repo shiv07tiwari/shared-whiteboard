dd9ec87e8c511aac743bfad79329c2c9eb43a004
Shivansh <shivansh16tiwari@gmail.com>
2020-05-01 15:53:42 +0530

    screenshots

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ function newConnection(socket) {
 
 	socket.on('mouse', mouseMsg);
 	socket.on('active', activeHandler);
+	
 	socket.on('marker', function(dump) {
 		socket.broadcast.emit('marker', "marker");
 	})
